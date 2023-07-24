60ed419b7b008513d4ce585e95bf18b65e12dc4b
Pranjal Mathur <pranjal.mathur.pm@gmail.com>
2023-07-24 19:16:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-def maxProfit(prices):
-    left = 0 #Buy
-    right = 1 #Sell
+def max_profit_func(prices):
+    left = 0
+    right = 0
     max_profit = 0
     while right < len(prices):
-        currentProfit = prices[right] - prices[left] #our current Profit
+        currect_profit = prices[right] - prices[left]
         if prices[left] < prices[right]:
-            max_profit =max(currentProfit,max_profit)
+            max_profit = max(currect_profit, max_profit)
         else:
             left = right
-        right += 1
+        right = right + 1
     return max_profit
